35abdd76c5509162ba0988bd5c95ba1f7c8f78b3
Keshav Sharma <keshavs642@gmail.com>
2025-01-17 21:45:25 +0530

    hql queries

--- src/main/java/com/telusko/App.java
+++ src/main/java/com/telusko/App.java
@@ -1,39 +1,87 @@
 package com.telusko;
 
+import jakarta.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
+import java.util.Random;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        AlienName name = new AlienName();
-        name.setFirstName("Keshav");
-        name.setMiddleName("Kumar");
-        name.setLastName("Sharma");
-
-        Alien keshav = new Alien();
-        keshav.setAid(101);
-        keshav.setAname(name);
-        keshav.setColor("Green");
-
-        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class);
-        ServiceRegistry serviceRegistry  = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry );
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Students.class);
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                                                .applySettings(configuration.getProperties())
+                                                .build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         Session session = sessionFactory.openSession();
 
-        // Save data in DB
         Transaction transaction = session.beginTransaction();
-            session.save(keshav);
-        transaction.commit();
 
-        // Fetch data from DB
-        // Will give us null if data does not exist in the DB
-        Alien fetcedAlien = session.get(Alien.class, 101);
-        System.out.println(fetcedAlien);
+              // Save 50 Students objects in DB
+//            Random r = new Random();
+//
+//            for(int i =1; i<=50; i++) {
+//                Students s = new Students();
+//                s.setRollno(i);
+//                s.setName("Name " + i);
+//                s.setMarks(r.nextInt(100));
+//                session.save(s);
+//            }
+
+            // Get all the rows from the Students table
+            Query q = session.createQuery("from Students");
+            List<Students> students = q.list();   // The list() method returns a list of Students
+
+            for(Students s : students) {
+                System.out.println(s);
+            }
+
+            // Get the students with marks > 50
+            Query greaterThan50Query = session.createQuery("from Students where marks > 50");
+            List<Students> studentsWithMarksGreaterThan50 = greaterThan50Query.list();   // The list() method returns a list of Students
+
+            for(Students s : studentsWithMarksGreaterThan50) {
+                System.out.println(s);
+            }
+
+            // Get a single row
+            Query singleRowResult = session.createQuery("from Students where rollno=7");
+            Students student = (Students) singleRowResult.uniqueResult();
+            System.out.println("Student with roll 7 : " + student);
+
+            // Get Student names only
+            Query studentNamesQuery = session.createQuery("select name from Students");
+            List<String> studentNames = studentNamesQuery.list();
+            studentNames.forEach(System.out::println);
+
+            // Get name and marks columns
+            // If we have data from multiple columns, the return type of the HQL query will be an array of objects where each object will be data from each column
+            Query nameAndMarksQuery = session.createQuery("select s.name, s.marks from Students s");
+            List<Object[]> namesAndMarks = nameAndMarksQuery.list();
+            namesAndMarks.forEach(e -> System.out.println(e[0] + " -> " + e[1]));
+
+            // Get sum of marks
+            Query sumOfMarksQuery = session.createQuery("select sum(s.marks) from Students s where s.marks > 60");
+            Long sumOfMarks = (Long) sumOfMarksQuery.uniqueResult();
+            System.out.println("Sum is " + sumOfMarks);
+
+            // Get only the students whose marks > 50
+            // Normally, this value "50" will come from a variable for dynamic behaviour.
+            int minMarks = 50;
+            Query newQuery = session.createQuery("from Students s where s.marks > :minMarks");
+            newQuery.setParameter("minMarks", minMarks);
+            List<Students> newResultList = newQuery.list();
+
+            System.out.println("Only the students with marks > " + minMarks);
+            newResultList.forEach(System.out::println);
+
+        transaction.commit();
     }
 }
---
+++ src/main/java/com/telusko/Students.java
@@ -0,0 +1,45 @@
+package com.telusko;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Students {
+    @Id
+    private int rollno;
+    private String name;
+    private int marks;
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "rollno=" + rollno +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
